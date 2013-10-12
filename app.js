f503352614f8182d93e009a5a611d511172fac09
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2013-10-12 22:26:36 +0530

    extra line breaks

--- app.js
+++ app.js
@@ -20,11 +20,11 @@ $(function() {
 					if (query == 'special') {
 						$('#results').append('<h2>Hot Offers!</h2>');
 						for (var i = 0; i < data[0].length; i++)
-							$('#results').append(data[0][i] + "<br>");
+							$('#results').append(data[0][i] + "<br><br>");
 						$('#results').append('<hr>');
 						$('#results').append('<h2>Latest Offers!</h2>');
 						for (var i = 0; i < data[1].length; i++)
-							$('#results').append(data[1][i] + "<br>");
+							$('#results').append(data[1][i] + "<br><br>");
 					} else if (query == 'dining') {
 						$('#results').append('<h2>Dining Offers!</h2>');
 						for (var i = 0; i < data.length; i++)
