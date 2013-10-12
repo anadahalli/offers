021ae7bc824ac289dc0e3561aab065ebbce94fc5
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2013-10-12 21:38:00 +0530

    index page

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Offers!</title>
+</head>
+<body>
+	<h1>Offers!</h1>
+	<form>
+		Bank
+		<select id="bank">
+			<option value="City">City Bank</option>
+		</select>
+		City
+		<select id="city">
+		</select>
+		Genre
+		<select id="genre">
+		</select>
+	</form>
+	<div id="results">
+	</div>
+	<script src="app.js"></script>
+</body>
+</html>
