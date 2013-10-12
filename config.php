309de0b5d0fb766fe6bba8b57ca156e6de05d330
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2013-10-12 21:23:32 +0530

    all the urls

---
+++ config.php
@@ -0,0 +1,47 @@
+<?php
+
+//~ all the urls
+$url_bank = array(
+
+	'City'		=> 'http://www.online.citibank.co.in',
+
+);
+
+$url_special = array(
+
+	'City'		=>	array(
+		
+		'Special Offers'	=>	'/card-offers/special-offers.htm',
+		
+		'Delhi / NCR'	=>	'/card-offers/delhi.htm#del',
+		'Mumbai' 		=> 	'/card-offers/mumbai.htm#mum',
+		'Pune'			=> 	'/card-offers/pune.htm',
+		'Ahmedabad' 	=> 	'/card-offers/ahmedabad.htm',
+		'Vadodara' 		=> 	'/card-offers/vadodara.htm',
+		'Bengaluru' 	=> 	'/card-offers/bengaluru.htm',
+		'Chennai' 		=> 	'/card-offers/chennai.htm',
+		'Hyderabad' 	=> 	'/card-offers/hyderabad.htm',
+		'Kolkata' 		=> 	'/card-offers/kolkata.htm',
+		
+	),
+);
+
+$url_dining = array(
+	
+	'City'		=>	array(
+	
+		'Delhi / NCR'	=>	'/card-offers/credit-card-delhi.htm',
+		'Mumbai' 		=> 	'/card-offers/credit-card-mumbai.htm',
+		'Pune'			=> 	'/card-offers/credit-card-pune.htm',
+		'Ahmedabad' 	=> 	'/card-offers/credit-card-ahmedabad.htm',
+		'Vadodara' 		=> 	'/card-offers/credit-card-vadodara.htm',
+		'Bengaluru' 	=> 	'/card-offers/credit-card-bengaluru.htm',
+		'Chennai' 		=> 	'/card-offers/credit-card-chennai.htm',
+		'Hyderabad' 	=> 	'/card-offers/credit-card-hyderabad.htm',
+		'Kolkata' 		=> 	'/card-offers/credit-card-kolkata.htm',
+			
+	),
+
+);
+
+?>
