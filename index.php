225e87fba25b6e60b20a10d060d46603c2ad25bd
Ashwath Nadahalli <ashwathnadahalli@gmail.com>
2013-10-12 22:21:58 +0530

    added all cities and genre

--- index.php
+++ index.php
@@ -12,11 +12,24 @@
 		</select>
 		City
 		<select id="city">
+			<option value="Delhi / NCR">Delhi</option>
+			<option value="Mumbai">Mumbai</option>
+			<option value="Pune">Pune</option>
+			<option value="Ahmedabad">Ahmedabad</option>
+			<option value="Vadodara">Vadodara</option>
+			<option value="Bengaluru">Bengaluru</option>
+			<option value="Chennai">Chennai</option>
+			<option value="Hyderabad">Hyderabad</option>
+			<option value="Kolkata">Kolkata</option>
 		</select>
 		Genre
 		<select id="genre">
+			<option value="special">Specials</option>
+			<option value="dining">Dining</option>
 		</select>
+		<input type="button" id="offers" value="Get Offers!">
 	</form>
+	<hr>
 	<div id="results">
 	</div>
 	<script src="jquery-2.0.3.min.js"></script>
